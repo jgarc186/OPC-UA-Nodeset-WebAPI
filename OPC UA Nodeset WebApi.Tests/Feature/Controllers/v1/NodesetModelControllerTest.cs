ae52729acc8b67095aa8f172c6ff375dcbb44924
Jose Garcia <jose.garcia@tthinkiq.com>
2025-07-11 10:29:14 -0400

    added two new feature tests to handle cases of different dependency versions

--- OPC UA Nodeset WebApi.Tests/Feature/Controllers/v1/NodesetModelControllerTest.cs
+++ OPC UA Nodeset WebApi.Tests/Feature/Controllers/v1/NodesetModelControllerTest.cs
@@ -79,4 +79,39 @@ public class NodesetModelControllerTest : TestBase
         Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
         Assert.Contains("http:opcfoundation.orgUAMachinery", await response.Content.ReadAsStringAsync());
     }
+
+    [Fact]
+    public async Task TestWhenUploadingXmlFromBase64WithImcompatableNodesetDependencyVersion_ShouldReturnInternalServerError()
+    {
+        await CreateProject();
+
+        // older version of the Base UA `1.04.7` and DI `1.03.0` Nodesets
+        await LoadNodesetModel("opcfoundation.org.UA.NodeSet2.xml");
+        await LoadNodesetModel("opcfoundation.org.UA.DI.NodeSet2.xml");
+
+        // Upload version of IA Nodeset `1.01.2`
+        await UploadXmlFromBase64("opcfoundation.org.IA.v.1.01.2.xml");
+
+        // Now load the latests version of the Machinery Nodeset `1.04.0`
+        var response = await UploadXmlFromBase64("opcfoundation.org.UA.Machinery.v.1.04.0.xml");
+
+        Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        Assert.Contains("Error: NodeSet can not be imported: Referenced node ns=1;i=480 not found for [ObjectType]MachineryOperationCounterType", await response.Content.ReadAsStringAsync());
+    }
+
+    [Fact]
+    public async Task TestWhenUploadingXmlFromBase64WithCompatableNodesetDependencyVersion_ShouldReturnOKStatus()
+    {
+        await CreateProject();
+
+        // Proper dependency versions: Base UA `1.05.03`, DI `1.04.0` & IA `1.01.2` Nodesets
+        await UploadXmlFromBase64("opcfoundation.org.UA.v.1.05.03.xml");
+        await UploadXmlFromBase64("opcfoundation.org.UA.DI.v.1.04.0.xml");
+        await UploadXmlFromBase64("opcfoundation.org.IA.v.1.01.2.xml");
+
+        // Now load the latests version of the Machinery Nodeset `1.04.0`
+        var response = await UploadXmlFromBase64("opcfoundation.org.UA.Machinery.v.1.04.0.xml");
+
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+    }
 }
