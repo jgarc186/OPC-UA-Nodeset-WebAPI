c7449812929a7fbf268d3726c8c22dd00d0c53a5
Jose Garcia <jose.garcia051@gmail.com>
2025-07-23 15:29:57 -0400

    working through adding new endpoint for reference types

---
+++ OPC UA Nodeset WebAPI/Controllers/v1/OpcTypesController.cs
@@ -0,0 +1,13 @@
+namespace OPC_UA_Nodeset_WebAPI.Controllers.v1;
+
+using CESMII.OpcUa.NodeSetModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Opc.Ua;
+using OPC_UA_Nodeset_WebAPI.Model.v1.Responses;
+using OPC_UA_Nodeset_WebAPI.Model.v1.Requests;
+using OPC_UA_Nodeset_WebAPI.UA_Nodeset_Utilities;
+using System.Web;
+
+public class OpcTypesController : AbstractBaseController
+{ }
